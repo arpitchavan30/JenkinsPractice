13e914829318f1aa4511892a493cffa88384f031
arpit <arpitchvn@gmail.com>
2024-04-03 15:28:32 +0530

    first commit

---
+++ src/main/java/com/example/JenkinsImplementation/JenkinsImplementationApplication.java
@@ -0,0 +1,14 @@
+package com.example.JenkinsImplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsImplementationApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to jenkins !!!");
+	}
+
+}
---
+++ src/test/java/com/example/JenkinsImplementation/JenkinsImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsImplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
