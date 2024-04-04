27a7fc51f8baa63c01c98aee26e7d978538c6527
arpit <arpitchvn@gmail.com>
2024-04-04 16:38:33 +0530

    second commit

--- src/main/java/com/example/JenkinsImplementation/JenkinsImplementationApplication.java
+++ src/main/java/com/example/JenkinsImplementation/JenkinsImplementationApplication.java
@@ -1,14 +1,17 @@
 package com.example.JenkinsImplementation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class JenkinsImplementationApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to jenkins !!!");
+		log.info("Working fine {}");
+		System.out.println("Welcome to Jenkins !!!");
 	}
 
 }
