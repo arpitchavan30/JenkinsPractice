de247f0a6511e008ab392dce76a6722cfc987dd5
arpitchavan30 <126240415+arpitchavan30@users.noreply.github.com>
2024-04-04 16:52:16 +0530

    Update JenkinsImplementationApplication.java

--- src/main/java/com/example/JenkinsImplementation/JenkinsImplementationApplication.java
+++ src/main/java/com/example/JenkinsImplementation/JenkinsImplementationApplication.java
@@ -8,7 +8,7 @@ public class JenkinsImplementationApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to jenkins !!!")
+		System.out.println("Welcome to jenkins !!!");
 	}
 
 }
