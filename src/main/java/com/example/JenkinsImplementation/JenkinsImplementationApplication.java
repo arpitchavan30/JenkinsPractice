dce56493bfec7b4441f229287fbda18a7fc76591
arpit <arpitchvn@gmail.com>
2024-05-02 12:25:38 +0530

    commit-2

--- src/main/java/com/example/JenkinsImplementation/JenkinsImplementationApplication.java
+++ src/main/java/com/example/JenkinsImplementation/JenkinsImplementationApplication.java
@@ -3,17 +3,21 @@ package com.example.JenkinsImplementation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Slf4j
+@RestController
 public class JenkinsImplementationApplication {
-
 	public static void main(String[] args) {
+		SpringApplication.run(JenkinsImplementationApplication.class, args);
+	}
 
-		log.info("Working fine {}");
-		System.out.println("Welcome to Jenkins !!!");
-		System.out.println("let's explore jenkins more..    ");
-		log.info("Finished !!! {} ");
+	@GetMapping
+	public String hello() {
+		System.out.println("Welcome");
+		return "hello";
 	}
 
 }
